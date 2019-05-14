e7a1a6fc27e10f6a8fd50fbc01f2f29dc2635f6f
jiimmy <y98765487@gmail.com>
2019-05-14 22:05:42 +0800

    組牌器版面配置大致完成

---
+++ chaos_deck_maker/chaos_deck_maker/chaos_deck_maker/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace chaos_deck_maker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ chaos_deck_maker/chaos_deck_maker/chaos_deck_maker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chaos_deck_maker.MvcApplication" Language="C#" %>
