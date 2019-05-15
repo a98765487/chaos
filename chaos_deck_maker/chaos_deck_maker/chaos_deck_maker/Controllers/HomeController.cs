683071f1087265e901707ad34d3f62ce303e9cec
jiimmy <y98765487@gmail.com>
2019-05-15 22:54:53 +0800

    右側搜尋大致完成

--- chaos_deck_maker/chaos_deck_maker/chaos_deck_maker/Controllers/HomeController.cs
+++ chaos_deck_maker/chaos_deck_maker/chaos_deck_maker/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entity;
+using Newtonsoft.Json;
 
 namespace chaos_deck_maker.Controllers
 {
@@ -11,6 +13,27 @@ namespace chaos_deck_maker.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult Search(FormCollection form)
+        {
+            var keyword = form["keyword"];
+            var page = form["page"];
+
+            if (String.IsNullOrEmpty(keyword) || String.IsNullOrEmpty(page))
+            {
+                return Json(new { Success = false });
+            }
+
+            var entityCard = new Card();
+            var cardPool = entityCard.Search(keyword, Convert.ToInt32(page));
+            if(cardPool.Count() > 0)
+            {
+                return Json(new { Success = true, CardPool = JsonConvert.SerializeObject(cardPool) });
+            }
+            else
+            {
+                return Json(new { Success = false });
+            }
         }
     }
 }
